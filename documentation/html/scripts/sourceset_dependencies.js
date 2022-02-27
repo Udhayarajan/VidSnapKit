bdd875412a5770dadf4a05eea37a603882d7ee2c
Udhayarajan <77388817+Udhayarajan@users.noreply.github.com>
2022-02-27 21:46:57 +0530

    Delete documentation/html directory

--- documentation/html/scripts/sourceset_dependencies.js
+++
@@ -1,1 +0,0 @@
-sourceset_dependencies='{":extractorlibrary:dokkaHtml/androidTestRelease":[],":extractorlibrary:dokkaHtml/debug":[],":extractorlibrary:dokkaHtml/main":[],":extractorlibrary:dokkaHtml/release":[]}'
